0856dfddc1d15967841c816ebcc3ea98bb54fcf7
vansh-bit <sharmavansh197900@gmail.com>
2026-06-07 03:19:21 +0530

    Allow all CORS origins unconditionally

--- server/src/app.js
+++ server/src/app.js
@@ -6,15 +6,7 @@ const app = express()
 
 app.use(cors({
     origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (
-            origin.endsWith('.vercel.app') || 
-            origin.startsWith('http://localhost:') || 
-            origin === process.env.CORS_ORIGIN
-        ) {
-            return callback(null, true);
-        }
-        return callback(new Error('Not allowed by CORS'));
+        callback(null, true);
     },
     methods: ["POST", "GET", "DELETE", "OPTIONS", "PATCH", "PUT"],
     credentials: true
