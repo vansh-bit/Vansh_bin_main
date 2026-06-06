55926bafa82697519d29498be9df7839fdea9070
vansh-bit <sharmavansh197900@gmail.com>
2026-06-07 03:26:33 +0530

    Add database connection startup logs

--- server/src/db/connection.js
+++ server/src/db/connection.js
@@ -4,6 +4,7 @@ import {DB_NAME} from "../constants.js";
 const connectDB = async ()=>{
     try
     {   
+        console.log(`Attempting to connect to MongoDB: ${process.env.MONGO_URI?.replace(/:([^@]+)@/, ":*****@")}/${DB_NAME}`);
         const connectionInstance =  await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
         console.log(`\nMongoDB connected  DB HOST: ${connectionInstance.connection.host}`);
     }
